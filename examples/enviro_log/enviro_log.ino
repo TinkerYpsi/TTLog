eef786abd76f6cce12eba2aa95d20968323c7235
Patrick Neggie <32044099+patrick9958@users.noreply.github.com>
2018-06-19 14:33:37 -0400

    added specific lux reader code

--- examples/enviro_log/enviro_log.ino
+++ examples/enviro_log/enviro_log.ino
@@ -4,6 +4,10 @@
 const int soilMoisturePin = A1;
 const int luxDetectorPin = A2;
 const int humiditySensorPin = 3;
+const int gc1Pin = 5;
+const int gc2Pin = 6;
+int gc1PinVal = 0;
+int gc2PinVal = 0;
 int lux = 0;
 int moisture = 0;
 String message;
@@ -17,6 +21,12 @@ void setup()
 {
   Serial.begin(9600);
   while(!Serial);
+
+  pinMode(gc1Pin, OUTPUT);
+  pinMode(gc2Pin, OUTPUT);
+  digitalWrite(gc1Pin, HIGH);
+  digitalWrite(gc2Pin, HIGH);
+
   Log.begin();
   Log.setDateTime();
   // TODO: only add once b/c we're appending to existing file
@@ -41,21 +51,11 @@ void loop()
   message += ",";
 
   lux = analogRead(luxDetectorPin);
-  moisture = analogRead(soilMoisturePin);
+  gc1PinVal = digitalRead(gc1Pin);
+  gc2PinVal = digitalRead(gc2Pin);
+  determineBrightness(lux, gc1PinVal, gc2PinVal, message);
 
-  if(lux > 700)
-  {
-    message += "Bright,";
-  }
-  else if(lux > 400)
-  {
-    message += "Medium,";
-  }
-  else
-  {
-    message += "Low,";
-  }
-  // TODO: check if removing commas won't change anything
+  moisture = analogRead(soilMoisturePin);
   if(moisture > 700)
   {
     message += "Damp,";
@@ -72,4 +72,52 @@ void loop()
   Log.entryCSV(message, filename);
   message = "";
   delay(5000);
+}
+
+void determineBrightness(int lux, int gc1PinVal, int gc2PinVal, String &message)
+{
+  // // L-gain: ~100,000 lux range
+  // if(gc1PinVal && gc2PinVal)
+  // {
+  //
+  // }
+  // // M-gain: ~10,000 lux range
+  // else if(gc2PinVal)
+  // {
+  //
+  // }
+  // // H-gain: ~1,000 lux range
+  // else
+  // {
+  //
+  // }
+  //
+  
+  lux *= 100;
+  message += String(lux);
+  message += ",";
+
+  // // 100,000 lx = 1023 analog output -- 1000 uA, 5000 ohm, 5V
+  // if(lux == 1023)
+  // {
+  //   message += "100000,";
+  // }
+  //
+  // // 10,000 lx = 102 analog output -- 100 uA, 5000 ohm, 0.5V (10% of 5V)
+  // if(lux == 102.3)
+  // {
+  //   message += "10000,";
+  // }
+  //
+  // // 1000 lx = 10 analog output -- 10 uA, 5000 ohm, 0.05V (1% of 5V)
+  // if(lux == 10.23)
+  // {
+  //   message += "1000,";
+  // }
+  //
+  // // 100 lx -- 1 uA, 5000 ohm, 0.005V (0.1% of 5V)
+  // if(lux == 1.023)
+  // {
+  //   message += "100,";
+  // }
 }
