faf87f9f63c4b9c63550a700b0a12bbd7598689c
Patrick Neggie <patar9958@users.noreply.github.com>
2018-06-19 17:39:42 -0400

    created detectLux() function

--- examples/enviro_log/enviro_log.ino
+++ examples/enviro_log/enviro_log.ino
@@ -21,15 +21,8 @@ void setup()
 {
   Serial.begin(9600);
   while(!Serial);
-
-  pinMode(gc1Pin, OUTPUT);
-  pinMode(gc2Pin, OUTPUT);
-  digitalWrite(gc1Pin, HIGH);
-  digitalWrite(gc2Pin, HIGH);
-
   Log.begin();
   Log.setDateTime();
-  // TODO: only add once b/c we're appending to existing file
   String header = "Humidity,Temperature,Light,Moisture";
   Log.entryHeaderCSV(header, filename);
 }
@@ -50,10 +43,9 @@ void loop()
   message += humidityDetector.temperature_F;
   message += ",";
 
-  lux = analogRead(luxDetectorPin);
-  gc1PinVal = digitalRead(gc1Pin);
-  gc2PinVal = digitalRead(gc2Pin);
-  determineBrightness(lux, gc1PinVal, gc2PinVal, message);
+  int lux = detectLux(gc1Pin, gc2Pin, luxDetectorPin);
+  message += String(lux);
+  message += ",";
 
   moisture = analogRead(soilMoisturePin);
   if(moisture > 700)
@@ -74,50 +66,72 @@ void loop()
   delay(5000);
 }
 
-void determineBrightness(int lux, int gc1PinVal, int gc2PinVal, String &message)
+int detectLux(int gc1Pin, int gc2Pin, int luxDetectorPin, bool debug = false)
 {
-  // // L-gain: ~100,000 lux range
-  // if(gc1PinVal && gc2PinVal)
-  // {
-  //
-  // }
-  // // M-gain: ~10,000 lux range
-  // else if(gc2PinVal)
-  // {
-  //
-  // }
-  // // H-gain: ~1,000 lux range
-  // else
-  // {
-  //
-  // }
-  //
-  
-  lux *= 100;
-  message += String(lux);
-  message += ",";
+  pinMode(gc1Pin, OUTPUT);
+  pinMode(gc2Pin, OUTPUT);
 
-  // // 100,000 lx = 1023 analog output -- 1000 uA, 5000 ohm, 5V
-  // if(lux == 1023)
-  // {
-  //   message += "100000,";
-  // }
-  //
-  // // 10,000 lx = 102 analog output -- 100 uA, 5000 ohm, 0.5V (10% of 5V)
-  // if(lux == 102.3)
-  // {
-  //   message += "10000,";
-  // }
-  //
-  // // 1000 lx = 10 analog output -- 10 uA, 5000 ohm, 0.05V (1% of 5V)
-  // if(lux == 10.23)
-  // {
-  //   message += "1000,";
-  // }
-  //
-  // // 100 lx -- 1 uA, 5000 ohm, 0.005V (0.1% of 5V)
-  // if(lux == 1.023)
-  // {
-  //   message += "100,";
-  // }
+  // set lux detector for low gain mode
+  digitalWrite(gc1Pin, HIGH);
+  digitalWrite(gc2Pin, HIGH);
+  int lux = analogRead(luxDetectorPin);
+  lux *= 100;
+
+  // if less than 1,000 lx, switch to high gain
+  if(lux < 1000)
+  {
+    // set lux detector for high gain mode
+    digitalWrite(gc1Pin, HIGH);
+    digitalWrite(gc2Pin, LOW);
+    lux = analogRead(luxDetectorPin);
+    if(debug)
+    {
+      Serial.println("HIGH GAIN MODE");
+      delay(1000);
+    }
+    return lux;
+  }
+
+  // if less than 10,000 lx, switch to medium gain
+  else if(lux < 10000)
+  {
+    // set lux detector for medium gain mode
+    digitalWrite(gc1Pin, LOW);
+    digitalWrite(gc2Pin, HIGH);
+    lux = analogRead(luxDetectorPin);
+    lux *= 10;
+    if(debug)
+    {
+      Serial.println("MEDIUM GAIN MODE");
+      delay(1000);
+    }
+    return lux;
+  }
+
+  // if greater than 10,000 lx, stay in high gain
+  else
+  {
+    if(debug)
+    {
+      Serial.println("LOW GAIN MODE");
+      delay(1000);
+    }
+    return lux;
+  }
 }
+
+// HIGH GAIN MODE
+// 100 lx = 102 analog output -- 100 uA, 5000 ohm, 0.5V (10% of 5V)
+// 10 lx = 10 analog output -- 10 uA, 5000 ohm, 0.05V (1% of 5V)
+// 1 lx = 1 analog output -- 1 uA, 5000 ohm, 0.005V (0.1% of 5V)
+
+// MEDIUM GAIN MODE
+// 100 lx = 10 analog output -- 10 uA, 5000 ohm, 0.05V (1% of 5V)
+// 1,000 lx = 102 analog output -- 100 uA, 5000 ohm, 0.5V (10% of 5V)
+// 10,000 lx = 1023 analog output -- 1000 uA, 5000 ohm, 5V
+
+// LOW GAIN MODE
+// 100 lx = 1 analog output -- 1 uA, 5000 ohm, 0.005V (0.1% of 5V)
+// 1,000 lx = 10 analog output -- 10 uA, 5000 ohm, 0.05V (1% of 5V)
+// 10,000 lx = 102 analog output -- 100 uA, 5000 ohm, 0.5V (10% of 5V)
+// 100,000 lx = 1023 analog output -- 1000 uA, 5000 ohm, 5V
--- examples/luxTest/luxTest.ino
+++ examples/luxTest/luxTest.ino
@@ -11,18 +11,81 @@ void setup()
 {
   Serial.begin(9600);
   while(!Serial);
+}
 
+void loop()
+{
+  int lux = detectLux(gc1Pin, gc2Pin, luxDetectorPin, true);
+  Serial.println(lux);
+}
+
+
+int detectLux(int gc1Pin, int gc2Pin, int luxDetectorPin, bool debug = false)
+{
   pinMode(gc1Pin, OUTPUT);
   pinMode(gc2Pin, OUTPUT);
 
   // set lux detector for low gain mode
   digitalWrite(gc1Pin, HIGH);
   digitalWrite(gc2Pin, HIGH);
+  int lux = analogRead(luxDetectorPin);
+  lux *= 100;
+
+  // if less than 1,000 lx, switch to high gain
+  if(lux < 1000)
+  {
+    // set lux detector for high gain mode
+    digitalWrite(gc1Pin, HIGH);
+    digitalWrite(gc2Pin, LOW);
+    lux = analogRead(luxDetectorPin);
+    if(debug)
+    {
+      Serial.println("HIGH GAIN MODE");
+      delay(1000);
+    }
+    return lux;
+  }
+
+  // if less than 10,000 lx, switch to medium gain
+  else if(lux < 10000)
+  {
+    // set lux detector for medium gain mode
+    digitalWrite(gc1Pin, LOW);
+    digitalWrite(gc2Pin, HIGH);
+    lux = analogRead(luxDetectorPin);
+    lux *= 10;
+    if(debug)
+    {
+      Serial.println("MEDIUM GAIN MODE");
+      delay(1000);
+    }
+    return lux;
+  }
+
+  // if greater than 10,000 lx, stay in high gain
+  else
+  {
+    if(debug)
+    {
+      Serial.println("LOW GAIN MODE");
+      delay(1000);
+    }
+    return lux;
+  }
 }
 
-void loop()
-{
-  lux = analogRead(luxDetectorPin);
-  lux *= 100;
-  Serial.println(lux);
-}
+// HIGH GAIN MODE
+// 100 lx = 102 analog output -- 100 uA, 5000 ohm, 0.5V (10% of 5V)
+// 10 lx = 10 analog output -- 10 uA, 5000 ohm, 0.05V (1% of 5V)
+// 1 lx = 1 analog output -- 1 uA, 5000 ohm, 0.005V (0.1% of 5V)
+
+// MEDIUM GAIN MODE
+// 100 lx = 10 analog output -- 10 uA, 5000 ohm, 0.05V (1% of 5V)
+// 1,000 lx = 102 analog output -- 100 uA, 5000 ohm, 0.5V (10% of 5V)
+// 10,000 lx = 1023 analog output -- 1000 uA, 5000 ohm, 5V
+
+// LOW GAIN MODE
+// 100 lx = 1 analog output -- 1 uA, 5000 ohm, 0.005V (0.1% of 5V)
+// 1,000 lx = 10 analog output -- 10 uA, 5000 ohm, 0.05V (1% of 5V)
+// 10,000 lx = 102 analog output -- 100 uA, 5000 ohm, 0.5V (10% of 5V)
+// 100,000 lx = 1023 analog output -- 1000 uA, 5000 ohm, 5V
